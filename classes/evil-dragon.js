6f7b842180f4c2cbb0cdb36e29314fea284823f5
Job Jean-Pierre Joseph <jobjpjoseph@gmail.com>
2023-12-02 02:49:24 -0500

    We satified all the tests.

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -1,1 +1,27 @@
 // Your code here
+const Dragon = require('./dragon');
+
+class EvilDragon extends Dragon {
+    constructor(name, color, evilDoings, nemesis) {
+        super(name, color);
+        this.evilDoings = evilDoings;
+        this.nemesis = nemesis;
+    }
+
+    dontInviteThemOverForDinner() {
+        for (let i = 0; i < this.evilDoings.length; i++) {
+            const doings = this.evilDoings[i];
+
+            console.log(`${this.name} will ${doings}`);
+        }
+
+        return;
+    }
+
+    burnsNemesis() {
+        return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+    }
+
+}
+
+module.exports = EvilDragon;
--- classes/friendly-dragon.js
+++ classes/friendly-dragon.js
@@ -1,1 +1,25 @@
 // Your code here
+const Dragon = require('./dragon');
+
+class FriendlyDragon extends Dragon {
+    constructor(name, color, lifeGoals, friend) {
+        super(name, color);
+        this.lifeGoals = lifeGoals; // => An array
+        this.friend = friend;
+    }
+
+    hasLifeGoals() {
+        for (let i = 0; i < this.lifeGoals.length; i++) {
+            const goals = this.lifeGoals[i];
+
+            console.log(`${this.name} likes to ${goals}`);
+        }
+        return;
+    }
+
+    helpsPeople() {
+        return `${this.name} helps their friend ${this.friend}`;
+    }
+}
+
+module.exports = FriendlyDragon;
